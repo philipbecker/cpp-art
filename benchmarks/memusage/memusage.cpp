b14ee45a1bcbf8203f0e86c77e236c4ae451b87c
Philip Becker-Ehmck <philipjr.becker@gmail.com>
2016-07-21 17:40:09 +0200

    Added basic memory usage comparison.

--- benchmarks/memusage/memusage.cpp
+++ benchmarks/memusage/memusage.cpp
@@ -2,7 +2,11 @@
 #include <ios>
 #include <iostream>
 #include <fstream>
-#include <string>
+#include <random>
+#include <map>
+#include <unordered_map>
+#include <art/radix_map.h>
+#include "../../libs/cpp-btree/btree_map.h"
 
 // Source: http://stackoverflow.com/a/671389/1311693
 
@@ -14,18 +18,17 @@
 //
 // On failure, returns 0.0, 0.0
 
-void process_mem_usage(double& vm_usage, double& resident_set)
-{
+void process_mem_usage(double &vm_usage, double &resident_set) {
     using std::ios_base;
     using std::ifstream;
     using std::string;
 
-    vm_usage     = 0.0;
+    vm_usage = 0.0;
     resident_set = 0.0;
 
     // 'file' stat seems to give the most reliable results
     //
-    ifstream stat_stream("/proc/self/stat",ios_base::in);
+    ifstream stat_stream("/proc/self/stat", ios_base::in);
 
     // dummy vars for leading entries in stat that we don't care about
     //
@@ -47,16 +50,47 @@ void process_mem_usage(double& vm_usage, double& resident_set)
     stat_stream.close();
 
     long page_size_kb = sysconf(_SC_PAGE_SIZE) / 1024; // in case x86-64 is configured to use 2MB pages
-    vm_usage     = vsize / 1024.0;
+    vm_usage = vsize / 1024.0;
     resident_set = rss * page_size_kb;
 }
 
-int main()
-{
+int main() {
     using std::cout;
     using std::endl;
 
     double vm, rss;
     process_mem_usage(vm, rss);
     cout << "VM: " << vm << "; RSS: " << rss << endl;
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<int64_t> dis(std::numeric_limits<int64_t>::min(),
+                                               std::numeric_limits<int64_t>::max());
+
+    std::vector<std::pair<int64_t, int64_t> > data;
+    const int size = 16000000;
+    for (int i = 0; i < size; i++) {
+        int64_t candidate = dis(gen);
+        data.emplace_back(candidate, i);
+    }
+
+    double key_value_size = size * sizeof(std::pair<int64_t, int64_t>);
+
+    double basline_vm, basline_rss;
+    process_mem_usage(basline_vm, basline_rss);
+    cout << "-- VM: " << basline_vm << "; RSS: " << basline_rss << endl;
+
+    {
+        //std::map<int64_t, int64_t> map;
+        //std::unordered_map<int64_t, int64_t> map;
+        //btree::btree_map<int64_t, int64_t> map;
+        art::radix_map<int64_t, int64_t> map;
+        for (auto &d : data)
+            map.insert(d);
+
+        double vm, rss;
+        process_mem_usage(vm, rss);
+        double byte_overhead = ((vm - basline_vm) * 1024) / ((double) size);
+        cout << "Overhead/Key in Byte: " << byte_overhead << endl;
+    }
 }
